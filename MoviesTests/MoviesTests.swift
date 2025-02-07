ad28d977b6912da82efe234548ce4dfbd71556e5
minamaged95 <mina.maged@talabat.com>
2025-02-07 09:40:58 +0200

    Initial Commit

---
+++ MoviesTests/MoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesTests.swift
+//  MoviesTests
+//
+//  Created by Mina Maged on 07/02/2025.
+//
+
+import Testing
+@testable import Movies
+
+struct MoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
